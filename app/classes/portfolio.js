2923451894aabefaab51dbb86370b2d86558faa3
Victor Ribero <victor.ribero3@gmail.com>
2018-02-17 23:47:02 +0100

    Prepared to creat a Profolio class

---
+++ app/classes/portfolio.js
@@ -0,0 +1,9 @@
+class Portfolio {
+  constructor(porfolioArray) {
+    coins = porfolioArray;
+    createdAt = new Date();
+  }
+
+  // I want to create a Prtfolio class to inject it to the Components.
+  // So they will execute methods of this class and nothing else
+}
